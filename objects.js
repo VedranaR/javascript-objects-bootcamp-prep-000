aad992214c32261b9dd0c2e6ceac48abd563e739
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-25 21:31:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,11 +5,9 @@ var playlist = {
 function updatePlaylist(playlist, artistName, title){
   playlist[artistName]=title;
   return playlist;
-  //should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.dartistName;
+  delete playlist.artistName;
   return playlist;
-
 }
